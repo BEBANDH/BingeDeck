6979605dbc1b68f069e94074966b146f0d3fc467
Bebandh <bebandhshrivastava5757@gmail.com>
2026-07-11 18:09:40 +0530

    Add TMDB API Key

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Store your secret keys here. 
 // This file is ignored by Git, meaning your keys won't be pushed to GitHub!
-const TMDB_API_KEY = 'YOUR_TMDB_API_KEY_HERE';
+const TMDB_API_KEY = '625ddaf394c404a5ebbe637ea42c7bcc';
